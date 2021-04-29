206246a3398679c8785619dc5e4a1e9f980230aa
bartaodi <barta.kod@gmail.com>
2021-04-29 12:55:41 +0200

    file exercise done

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,31 @@
+
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) throw new IllegalArgumentException("Üres vagy nem található fájl.");
+        File file = new File(output);
+        BufferedWriter bf;
+
+        try {
+            file.delete();
+            file.createNewFile();
+            bf = new BufferedWriter(new FileWriter(file));
+            for (Map.Entry<String, String> entry :
+                    contacts.entrySet()) {
+                bf.write(entry.getKey() + ": "
+                        + entry.getValue());
+                bf.newLine();
+            }
+            bf.flush();
+            bf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
